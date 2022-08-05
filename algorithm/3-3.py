3d9eaa2a8208986280aa3b1a3ed4355ea00b5873
syoake <ryupark1006@gmail.com>
2022-08-05 13:13:20 +0900

    [algorithm] 3-3 숫자 카드 게임

--- algorithm/3-3.py
+++ algorithm/3-3.py
@@ -1,13 +1,27 @@
-n, m = map(int, input().split)
+n, m = map(int, input().split())
 
-answer = 0
+result = 0
 
+# min() 함수를 이용하는 방법
 # 한 줄씩 입력받아 확인
 for i in range(n):
-    data = list(map(int, input().split))
+    data = list(map(int, input().split()))
     # 현재 줄에서 가장 작은 수 찾기
     min_value = min(data)
     # 가장 작은 수들 중에서 가장 큰 수 찾기
-    answer = max(answer, min_value)
+    result = max(result, min_value)
 
-print(answer)
+print(result)
+
+# 이중 반복문 구조를 이용하는 방법
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # 가장 작은 수들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+print(result)
