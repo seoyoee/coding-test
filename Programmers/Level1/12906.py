a76e22da6ad780a2ffa00dc55a5e4d143e3d282d
syoake <ryupark1006@gmail.com>
2022-09-07 12:04:55 +0900

    [PGS] 12906 같은 숫자는 싫어

--- Programmers/Level1/12906.py
+++ Programmers/Level1/12906.py
@@ -1,13 +1,9 @@
-from collections import deque
-
 def solution(arr):
     answer = []
-    queue = deque()
     
     for n in arr:
-        if (not queue) or (n != num):
-            queue.append(n)
+        if (not answer) or (n != num):
+            answer.append(n)
             num = n
     
-    answer = list(queue)
     return answer
