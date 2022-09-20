40847dce70378655eb6fb7b5ed422271383a23fb
Seoyoung <ryupark1006@gmail.com>
2022-09-20 11:17:18 +0900

    [PGS] 77884 약수의 개수와 덧셈

---
+++ Programmers/Level1/77884.py
@@ -0,0 +1,8 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if (i ** (1/2)) % 1:
+            answer += i
+        else:
+            answer -= i
+    return answer
